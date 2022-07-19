115ebf896eb9684582006854a2c5950001226558
Hariket Parmar <p.hariket2002@gmail.com>
2022-07-19 15:21:36 +0530

    [ADD] employee_ksc: add new module

---
+++ employee_ksc/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Employee Ksc',
+    'version': '15.1.2.1.1',
+    'summary': '',
+    'description': """""",
+    'category': '',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/emp_views.xml',
+        'data/emp_ksc_data.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
